27fdc0a7fe75da4279df296cdb7c54d5c51454ad
Dane <danejwhitfield@gmail.com>
2022-04-04 19:11:45 +0100

    WIP - updating game modes

--- singleplayer.js
+++ singleplayer.js
@@ -3,110 +3,65 @@ const divInnerText = divGridElement.map((x) => x.innerText);
 const winnerText = document.querySelector(".winner");
 const currentPlayer = document.querySelector(".current-player");
 
-let playerSymbol = Math.floor(Math.random() * 2);
-playerSymbol === 0 ? (playerSymbol = "✘") : (playerSymbol = "☮");
-let computerSymbol;
-playerSymbol === "✘" ? (computerSymbol = "☮") : (computerSymbol = "✘");
+// RANDOMLY CHOOSE SYMBOLS
+let symbols = ["☮", "✘"];
+let playersSymbol;
+let computersSymbol;
+playersSymbol = symbols[Math.floor(Math.random() * 2)];
+playersSymbol === "☮"
+  ? (computersSymbol = symbols[1])
+  : (computersSymbol = symbols[0]);
+//
 
-let arr = [];
-let isWinner = false;
-const checkWinningLine = () => {
-  const winners = [
-    [0, 1, 2],
-    [0, 3, 6],
-    [0, 4, 8],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 4, 5],
-    [6, 7, 8],
-    [2, 4, 6],
-  ];
-
-  if (isWinner === false) {
-    winners.forEach((winner) => {
-      winner.forEach(() => {
-        const firstValue = arr[winner[0]];
-        if (firstValue) {
-          if (firstValue === arr[winner[1]] && firstValue === arr[winner[2]]) {
-            winnerText.innerText = `${firstValue} wins!`;
-            isWinner = true;
-            divGridElement[winner[0]].style.background = "#90EE90";
-            divGridElement[winner[1]].style.background = "#90EE90";
-            divGridElement[winner[2]].style.background = "#90EE90";
-          }
-        }
-      });
-    });
-  }
-};
-
-const makeDumbMove = () => {
-  for (let i = 0; i < 1; i++) {
-    if (!divGridElement[i].innerText) {
-      const randomSquare = Math.floor(Math.random() * 9);
-      divGridElement[randomSquare].innerText = computerSymbol;
-    }
-  }
-};
-
-function computerMove() {
-  currentPlayer.innerText = "Computer Turn";
-  setTimeout(() => {
-    //makeSmartMove()
-    makeDumbMove();
-  }, 1000);
-  checkWinningLine();
-  playerMove();
-}
-
-function playerMove() {
-  currentPlayer.innerText = "Your Turn";
-  divGridElement.forEach((div, i) =>
-    div.addEventListener("click", () => {
-      if (!div.innerText) {
-        div.innerText = currentTurn;
-        arr[i] = div.innerText;
-      }
-    })
-  );
-
-  checkWinningLine();
-  computerMove();
-}
-
+// START GAME FUNC
 const startGame = () => {
-  let playerScore = 0;
-  let computerScore = 0;
-
-  let goesFirst = Math.floor(Math.random() * 2);
-  if (goesFirst === 0) {
-    playerMove();
-  } else {
-    computerMove();
-  }
-
-  /*  let currentTurn = '✘';
+  let currentTurn = "✘";
   const changeTurn = () => {
-    if (currentTurn === '✘') {
-      currentTurn = '☮';
+    if (currentTurn === "✘") {
+      currentTurn = "☮";
     } else {
-      currentTurn = '✘';
+      currentTurn = "✘";
     }
-  }; */
+  };
 
-  /*  divGridElement.forEach((div, i) =>
-  div.addEventListener("click", () => {
-    if (!div.innerText) {
-        div.innerText = currentTurn;
-        arr[i] = div.innerText;
-        changeTurn();
-        checkWinningLine();
-        makeMove();
+  // RANDOMLY CHOOSE WHO PLAYS FIRST
+  const firstTurn = Math.floor(Math.random() * 2);
+  if (firstTurn === 0) {
+    currentPlayer.innerText = "Your Turn";
+    function playersMove() {
+      divGridElement.forEach((div, i) =>
+        div.addEventListener("click", () => {
+          if (!div.innerText) {
+            div.innerText = currentTurn;
+            arr[i] = div.innerText;
+            changeTurn();
+            checkWinningLine();
+            computersMove();
+          }
+        })
+      );
+    }
+  } else {
+    currentPlayer.innerText = "Computer's Turn";
+    function computersMove() {
+      for (let i = 0; i < 1; i++) {
+        console.log("divGridElement:", divGridElement);
+        divGridElement[i].addEventListener("click", () => {
+          if (!divGridElement[i].innerText) {
+            div.innerText = currentTurn;
+            arr[i] = div.innerText;
+            changeTurn();
+            checkWinningLine();
+            playersMove();
+          }
+        });
       }
-    })
-    ); */
-  let arr = [];
+    }
+  }
+  //
+
   let isWinner = false;
+  let arr = [];
 
   const checkWinningLine = () => {
     const winners = [
@@ -140,8 +95,18 @@ const startGame = () => {
       });
     }
   };
-};
 
+  // divGridElement.forEach((div, i) =>
+  //   div.addEventListener("click", () => {
+  //     if (!div.innerText) {
+  //       div.innerText = currentTurn;
+  //       arr[i] = div.innerText;
+  //       changeTurn();
+  //       checkWinningLine();
+  //     }
+  //   })
+  // );
+};
 startGame();
 
 const reset = () => {
---
+++ singleplayerX.js
@@ -0,0 +1,94 @@
+const divGridElement = [...document.querySelectorAll("div")];
+const divInnerText = divGridElement.map((x) => x.innerText);
+const winnerText = document.querySelector(".winner");
+const currentPlayer = document.querySelector(".current-player");
+
+let playerSymbol = Math.floor(Math.random() * 2);
+playerSymbol === 0 ? (playerSymbol = "✘") : (playerSymbol = "☮");
+let computerSymbol;
+playerSymbol === "✘" ? (computerSymbol = "☮") : (computerSymbol = "✘");
+
+let arr = [];
+let isWinner = false;
+
+const checkWinningLine = () => {
+  const winners = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 4, 5],
+    [6, 7, 8],
+    [2, 4, 6],
+  ];
+
+  if (!isWinner) {
+    winners.forEach((winner) => {
+      winner.forEach(() => {
+        const firstValue = arr[winner[0]];
+        if (firstValue) {
+          if (firstValue === arr[winner[1]] && firstValue === arr[winner[2]]) {
+            winnerText.innerText = `${firstValue} wins!`;
+            isWinner = true;
+            divGridElement[winner[0]].style.background = "#90EE90";
+            divGridElement[winner[1]].style.background = "#90EE90";
+            divGridElement[winner[2]].style.background = "#90EE90";
+          }
+        }
+      });
+    });
+  }
+};
+
+const makeDumbMove = () => {
+  for (let i = 0; i < 1; i++) {
+    if (!divGridElement[i].innerText) {
+      const randomSquare = Math.floor(Math.random() * 9);
+      divGridElement[randomSquare].innerText = computerSymbol;
+    }
+  }
+};
+
+function computerMove() {
+  currentPlayer.innerText = "Computer Turn";
+  // setTimeout(() => {
+  //   makeDumbMove();
+  // }, 1000);
+  makeDumbMove();
+  // checkWinningLine();
+  playerMove();
+}
+
+function playerMove() {
+  currentPlayer.innerText = "Your Turn";
+  divGridElement.forEach((div, i) =>
+    div.addEventListener("click", () => {
+      if (!div.innerText) {
+        div.innerText = currentTurn;
+        arr[i] = div.innerText;
+      }
+    })
+  );
+  // checkWinningLine();
+  computerMove();
+}
+
+const startGame = () => {
+  let playerScore = 0;
+  let computerScore = 0;
+
+  let goesFirst = Math.floor(Math.random() * 2);
+  if (goesFirst === 0) {
+    playerMove();
+  } else {
+    computerMove();
+  }
+};
+
+startGame();
+
+const reset = () => {
+  location.reload();
+  startGame();
+};
